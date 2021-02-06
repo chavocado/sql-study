d56aba7789c9c0d4d2e703a4f95a1f4fa98be7d4
Daniel Chavez <d.chavo562@gmail.com>
2021-02-06 16:15:32 -0600

    beginner SQL structures

--- modestudy.sql
+++ modestudy.sql
@@ -40,3 +40,40 @@ SELECT *
 SELECT *
   FROM tutorial.us_housing_units
   WHERE west <= 20 
+   --NOTE: SQL uses single quotes to reference column values.
+ --6. Write a query that only shows rows for which the month name is February.
+SELECT *
+    FROM tutorial.us_housing_units
+    WHERE month_name = 'February'
+ --7.Write a query that only shows rows for which the month_name starts with the letter "N" or an earlier letter in the alphabet.
+ SELECT *
+    FROM tutorial.us_housing_units
+    WHERE month_name < 'o'
+--8. Write a query that calculates the sum of all four regions in a separate column.
+SELECT year,
+       month,
+       south,
+       west,
+       midwest,
+       northeast,
+       south + west + midwest + northeast as "Total"
+    FROM tutorial.us_housing_units
+--9. Write a query that returns all rows for which more units were produced in the West region than in the Midwest and Northeast combined.
+SELECT year,
+       month,
+       south,
+       west,
+       midwest,
+       northeast,
+    FROM tutorial.us_housing_units
+    WHERE west > (midwest + northeast)
+--10. Write a query that calculates the percentage of all houses completed in the United States represented by each region. Only return results from the year 2000 and later.
+--HINT: There should be four columns of percentages.
+SELECT year,
+       month,
+       south/(south+west+midwest+northeast) * 100 as "South Pct",
+       west/(south+west+midwest+northeast) * 100 as "West Pct",
+       midwest/(south+west+midwest+northeast) * 100 as "MidWest Pct",
+       northeast/(south+west+midwest+northeast) * 100 as "NE Pct"
+    FROM tutorial.us_housing_units
+    WHERE year <= 2000
